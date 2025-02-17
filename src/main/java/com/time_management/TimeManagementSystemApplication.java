fd05594a713006e8843c78fd1aca82c3a9bf0462
san <sunvhs@gmail.com>
2025-02-17 15:35:21 -0300

    First commit

---
+++ src/main/java/com/time_management/TimeManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.time_management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimeManagementSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TimeManagementSystemApplication.class, args);
+    }
+
+}
