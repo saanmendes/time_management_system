97dba8078f95e419f48e93e60656e966a4dfdb00
san <sunvhs@gmail.com>
2025-02-24 16:04:14 -0300

    Add implementation for service

--- src/main/java/com/time_management/infra/output/repositories/ReportRepository.java
+++ src/main/java/com/time_management/infra/output/repositories/ReportRepository.java
@@ -1,7 +1,10 @@
 package com.time_management.infra.output.repositories;
 
 import com.time_management.domain.models.Report;
+import com.time_management.infra.output.entities.ReportEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-public interface ReportRepository extends JpaRepository<Report, String> {
+@Repository
+public interface ReportRepository extends JpaRepository<ReportEntity, String> {
 }
