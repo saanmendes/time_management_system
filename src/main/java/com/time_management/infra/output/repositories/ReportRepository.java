22b03c234f0c562f25514a6cf3d4aaefb83590f4
Samira Rodrigues Silva <srs.samira.silva@gmail.com>
2025-02-24 18:17:53 -0300

    modify repositories to use in report use case

--- src/main/java/com/time_management/infra/output/repositories/ReportRepository.java
+++ src/main/java/com/time_management/infra/output/repositories/ReportRepository.java
@@ -1,7 +1,16 @@
 package com.time_management.infra.output.repositories;
 
-import com.time_management.domain.models.Report;
+import com.time_management.infra.output.entities.ReportEntity;
+import com.time_management.infra.output.entities.TaskEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
-public interface ReportRepository extends JpaRepository<Report, String> {
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Repository
+public interface ReportRepository extends JpaRepository<ReportEntity, String> {
+
 }
--- src/main/java/com/time_management/infra/output/repositories/TaskRepository.java
+++ src/main/java/com/time_management/infra/output/repositories/TaskRepository.java
@@ -3,7 +3,13 @@ package com.time_management.infra.output.repositories;
 import com.time_management.domain.models.Task;
 import com.time_management.infra.output.entities.TaskEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.time.LocalDateTime;
+import java.util.List;
 
 public interface TaskRepository extends JpaRepository<TaskEntity, String> {
-
+    @Query("SELECT t FROM TaskEntity t WHERE t.initialDate >= :startDate")
+    List<TaskEntity> findByInitialDate(@Param("startDate") LocalDateTime startDate);
 }
