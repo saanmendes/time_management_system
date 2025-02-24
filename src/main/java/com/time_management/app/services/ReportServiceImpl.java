f474818363da2699618383572df1ba9b16678100
Samira Rodrigues Silva <srs.samira.silva@gmail.com>
2025-02-24 20:02:10 -0300

    add generateReport method to ReportService with inject dependency

--- src/main/java/com/time_management/app/services/ReportServiceImpl.java
+++ src/main/java/com/time_management/app/services/ReportServiceImpl.java
@@ -1,8 +1,21 @@
 package com.time_management.app.services;
 
+import com.time_management.app.dtos.reports.ReportResponseDTO;
 import com.time_management.app.ports.ReportService;
+import com.time_management.domain.usecases.ReportUseCase;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ReportServiceImpl implements ReportService {
+
+   private final ReportUseCase reportUseCase;
+
+   public ReportServiceImpl(ReportUseCase reportUseCase) {
+       this.reportUseCase = reportUseCase;
+   }
+
+   public ReportResponseDTO generateReport(int days) {
+       return reportUseCase.generateReport(days);
+   }
+
 }
