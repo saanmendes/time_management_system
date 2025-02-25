a0fc783bddb47bb0246a3dd68215dcbb2ffa6372
Samira Rodrigues Silva <162381123+srssamira@users.noreply.github.com>
2025-02-25 13:52:38 -0300

    Update src/main/java/com/time_management/app/ports/ReportService.java
    Co-authored-by: Ana Laura <89489383+anabergerr@users.noreply.github.com>

--- src/main/java/com/time_management/app/ports/ReportService.java
+++ src/main/java/com/time_management/app/ports/ReportService.java
@@ -3,6 +3,5 @@ package com.time_management.app.ports;
 import com.time_management.app.dtos.reports.ReportResponseDTO;
 
 public interface ReportService {
-
     public ReportResponseDTO generateReport(int days);
 }
